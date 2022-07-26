7c8ac6841c2e44ee252c5430c2bc46320ede1778
luis.map <luismap2508@gmail.com>
2022-07-26 09:30:03 +0200

    [feat] removing checks by adding 2 more elements to array

--- src/main/scala/tabulation/Fib.scala
+++ src/main/scala/tabulation/Fib.scala
@@ -8,33 +8,34 @@ import scala.collection.mutable.ArrayBuffer
 object Fib extends App {
 
   val logger = Logger.getLogger(this.getClass.getName)
-  val canlog = false
+  val canlog = true
+
   /**
    * tabulation.
    * - use a table to represent solution
    * - init table(in our case an array)
    * - init base cases
-    */
+   */
 
-    //O(n) time
-    //O(n) space
-  def fibTab (n: Int): Int = {
-    val array = new Array[Int](n + 1) //init table
-    if(canlog)logger.info(s"Array ${array.toList}")
+  //O(n) time
+  //O(n) space
+  def fibTab(n: Int): Int = {
+    val array = new Array[Int](n + 2) //init table
+    if (canlog) logger.info(s"Array ${array.toList}")
     array(1) = 1 //init base case
-    for (idx <- 0 to n) {
-      if(idx + 1 <= n) array(idx + 1) += array(idx)
-      if(idx + 2 <= n) array(idx + 2) += array(idx)
+    for (idx <- 0 until  n) {
+      array(idx + 1) += array(idx)
+      array(idx + 2) += array(idx)
       if (canlog) logger.info(s"working indx $idx ${array.toList}")
     }
 
-    if(canlog)logger.info(s"Array ${array.toList}")
+    if (canlog) logger.info(s"Array ${array.toList}")
     array(n)
   }
 
-  val n = 52
+  val n = 6
   val fibN = timer(fibTab(n), "fib_lineal", logger)
 
 
-  logger.info(s"[fib] fib of $fibN = $n ")
+  logger.info(s"[fib] fib of $n = $fibN ")
 }
